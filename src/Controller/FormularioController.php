7767799fedec653f45ee2285bdd630d3172b77e4
dramostr <dan.ramostr@gmail.com>
2023-06-20 02:16:50 -0400

    creacion de formulario de votacion

--- src/Controller/FormularioController.php
+++ src/Controller/FormularioController.php
@@ -8,11 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FormularioController extends AbstractController
 {
-    #[Route('/formulario', name: 'app_formulario')]
+    #[Route('formulario/list', name: 'app_list')]
     public function index(): Response
     {
         return $this->render('formulario/index.html.twig', [
             'controller_name' => 'FormularioController',
         ]);
     }
+
+    #[Route('formulario/create', name: 'app_create')]
+    public function create(): Response
+    {
+        return $this->render('formulario/create.html.twig', [
+            'controller_name' => 'FormularioController',
+        ]);
+    }
+
 }
